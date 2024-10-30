15af913ff0c31ed8448f76ca7089b8d6eff67a4a
ismanong <526443791@qq.com>
2024-10-31 03:09:22 +0800

    1

--- panpan/js/index.js
+++ panpan/js/index.js
@@ -1,25 +1,28 @@
 document.body.addEventListener('click', function (event) {
-    if (event.target.className.indexOf('popup-button-') > -1) {
-        var num = event.target.className.split('popup-button-').pop()
-        var isLeft = Number(num) < 6
-        Swal.fire({
-            showConfirmButton: false,
-            imageUrl: "./img/index_popup_" + num + ".png",
-            width: window.innerWidth * 0.8,
-            showClass: {
-                popup: `
+  if (event.target.className.indexOf('popup-button-') > -1) {
+    var num = event.target.className.split('popup-button-').pop()
+    var isLeft = Number(num) < 6
+    Swal.fire({
+      showConfirmButton: false,
+      imageUrl: "./img/index_popup_" + num + ".png",
+      width: window.innerWidth * 0.8,
+      showClass: {
+        popup: `
               animate__animated
               ${isLeft ? 'animate__bounceInLeft' : 'animate__bounceInRight'}
               animate__faster
             `
-            },
-            hideClass: {
-                popup: `
+      },
+      hideClass: {
+        popup: `
               animate__animated
               ${isLeft ? 'animate__bounceOutLeft' : 'animate__bounceOutRight'}
               animate__faster
             `
-            },
-        })
-    }
+      },
+    })
+  }
+  if (event.target.className.indexOf('index_info') > -1) {
+    location.href = './info/index.html';
+  }
 });
